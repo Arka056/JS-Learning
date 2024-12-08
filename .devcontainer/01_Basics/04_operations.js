f1b7ddeb4eb751fcee3469234b2bc8d67f95e375
Arka Kundu <arka99325@gmail.com>
2024-12-08 07:05:27 +0000

    Different operations

---
+++ .devcontainer/01_Basics/04_operations.js
@@ -0,0 +1,23 @@
+let val = 3
+let negVal = -val
+console.log(negVal) // returns -3
+console.log("Sum is: "+ (2+2))  // returns 4
+console.log("Subtraction: "+ (2-2)) // returns 0
+console.log("Multiplication: "+ (2*2)) // returns 4
+console.log("Division: "+ (2/2)) // returns 1
+console.log("Remainder: "+ (2%2)) // returns 0
+console.log("Power of 2: "+ (5**2)) // returns 25
+
+let str = "hi"
+let st2 = "Arka"
+let st = str+st2
+console.log("Concatenated String: " + st) // returns hiArka
+
+console.log(1+2) // returns 3
+console.log(1+"2") // returns 12
+console.log("1"+8+2) // returns 182
+console.log(1+8+"2") // returns 92
+console.log(1+"8"+2) // returns 182
+console.log(+true); // true is converted to
+console.log(+""); // returns 0 : null is converted to 0 (number)
+
