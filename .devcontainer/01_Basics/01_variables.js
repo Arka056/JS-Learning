faa4d9e0336e873279044e7284f0b02bdefcacdb
Arka Kundu <arka99325@gmail.com>
2024-12-08 04:27:28 +0000

    datatypes

---
+++ .devcontainer/01_Basics/01_variables.js
@@ -0,0 +1,12 @@
+const account_Id =  144553
+let accountEmail = "arka99325@gmail.com"
+var accountPassword = "12345"
+accountCity = "Kolkata"
+let accountState;
+// account_Id = 1222 -> Not Allowed
+console.log(account_Id)
+accountEmail = "abc@abc.com"
+accountPassword = "1290938"
+accountCity = "Delhi"
+console.table([account_Id, accountEmail, accountPassword, accountCity, accountState])
+
---
+++ .devcontainer/01_Basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+"user strict"; // treat all JS code as newer version
+// alert(2+2) // Showing error since we are using nodejs not the browser
+let name ="Arka"
+let age = 21
+let hello;
+let hello1 =null;
+let isArka = true
+console.log(typeof(isArka)) // returns boolean
+console.log(typeof(hello)) // returns undefined
+console.log(typeof(hello1)) // returns object
+
+// number -> 2^53
+// bigint
+// string -> ""
+// boolean -> true/false
+// null -> standalone value
+// undefined -> 
