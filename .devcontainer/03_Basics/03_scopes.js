5c0205fd1eac19095816a85deb675b65187559ef
Arka Kundu <arka99325@gmail.com>
2024-12-12 16:19:08 +0000

    03_scopes

---
+++ .devcontainer/03_Basics/03_scopes.js
@@ -0,0 +1,8 @@
+{
+    let a = 12
+const b = 23
+var c = 11
+}
+// console.log(a); // returns ReferenceError: a is not defined
+// console.log(b); // returns ReferenceError: b is not defined
+console.log(c); // retuns 11 inspite of scope
